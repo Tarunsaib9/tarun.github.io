dc0ad87a16336078f2a64f20cf33f40212cacd32
Syrus Dark <naveen@syrusdark.website>
2020-03-20 16:05:13 +0530

    Some syntax error changes

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -9,6 +9,9 @@ import Achievement from "./achievement/Achievement";
 import Blogs from "./blogs/Blogs";
 import Contact from "./contact/Contact";
 import Footer from "../components/footer/Footer";
+import Talks from "./talks/Talks";
+import Podcast from "./podcast/Podcast";
+import Top from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 
 export default class Main extends Component {
@@ -23,9 +26,12 @@ export default class Main extends Component {
         <StartupProject />
         <Achievement />
         <Blogs />
+        <Talks />
 	      <Twitter />
+        <Podcast />
         <Contact />
         <Footer />
+        <Top />
       </div>
     );
   }
